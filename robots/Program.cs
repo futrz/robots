ef871b68efe669820c6618c7b4985b4b64333e84
futrz <almi2@op.pl>
2017-09-18 22:41:53 +0200

    funkcje warunki i petle

--- robots/Program.cs
+++ robots/Program.cs
@@ -10,6 +10,81 @@ namespace robots
     {
         static void Main(string[] args)
         {
+            //int cyferka = 3;
+            //cyferka = Zwieksz(cyferka) + Zwieksz(cyferka);
+            //Console.WriteLine(cyferka);
+            NapiszCzasem(3);
+            NapiszCzasem(7);
+
+            int i;
+            i = 0;
+            while(i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            for(i=0; i<10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
+
+        static void NapiszCzasem(int a)
+        {
+            if (a > 4)
+                Console.WriteLine(a);
+            else
+                Console.WriteLine(-a);
+        }
+
+        static int Zwieksz(int a)
+        {
+            a = a + 1;
+            return a;
+        }
+
+        static int Osiem()
+        {
+            return 8;
+        }
+
+        static void Napisz(string tekst)
+        {
+            Console.WriteLine(tekst);
+        }
+
+        static void Napisz(int liczba)
+        {
+            string zamienione = liczba.ToString();
+            Console.WriteLine(zamienione);
+        }
+
+        static void NapiszWiecej(int wiecejcyferek)
+        {
+            int niebardzowiadomoco = wiecejcyferek + 1;
+            Napisz(niebardzowiadomoco);
+        }
+
+        static void Schowek()
+        {
+            string cos = "jakies cokolwiek";
+            Console.WriteLine(cos);
+            cos = "jakis inny napis";
+            Console.WriteLine(cos);
+            Console.WriteLine("cos");
+            Napisz("abc");
+            Napisz("def");
+            int numerek = 7;
+            Console.WriteLine(numerek);
+            Napisz(numerek);
+            NapiszWiecej(numerek);
+            Console.ReadKey();
+        }
+
+        // pokazać zwracanie wartości
     }
+
 }
